51dfca49448f7d9d46536d8dd622c781a3435dad
hogrider301 <81501664+hogrider301@users.noreply.github.com>
2022-10-03 01:33:51 +0530

    Create sympy.py

---
+++ python/sympy.py
@@ -0,0 +1,10 @@
+# Python program to check prime number
+# using sympy.isprime() method
+
+# importing sympy module
+from sympy import *
+
+# calling isprime function on different numbers
+isprime(30)
+isprime(13)
+isprime(2)
