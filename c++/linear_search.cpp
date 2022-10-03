b375b4349a42b95e1cf77a37ed15612119784b31
Dartpixel <skartiksharma2004@gmail.com>
2022-10-03 19:23:11 +0530

    sorting

---
+++ c++/linear_search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int linearSearch(int arr[], int n, int key)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, key;
+    cout << "Enter the limit" << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the element to be searched" << endl;
+    cin >> key;
+    cout << linearSearch(arr, n, key);
+    return 0;
+}
